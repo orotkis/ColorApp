d2c7c39e613062e5367aea34a91251df2fd2184a
orotkis <orotkis@gmail.com>
2020-08-28 20:31:02 -0500

    Added more icons for the FAB options

--- app/src/main/java/com/example/colorapp/MainActivity.java
+++ app/src/main/java/com/example/colorapp/MainActivity.java
@@ -19,28 +19,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFab();
 
+    }
+
+    //sets up FAB for new clothing item menu
+    private void initFab(){
         //FAB to open new clothing item menu
         SpeedDialView speedDialView = findViewById(R.id.speedDial);
-        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_footwear, R.drawable.ic_link_white_24dp).create());
-        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_bottom, R.drawable.ic_link_white_24dp).create());
+        //populating FAB with options
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_footwear, R.drawable.shoe_formal).create());
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_bottom, R.drawable.seat_legroom_extra).create());
         speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_top, R.drawable.tshirt_crew).create());
-        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_outerwear, R.drawable.ic_link_white_24dp).create());
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_add_outerwear, R.drawable.snowflake).create());
         speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
             @Override
             public boolean onActionSelected(SpeedDialActionItem speedDialActionItem) {
                 switch (speedDialActionItem.getId()) {
                     case R.id.fab_add_outerwear:
                         Log.d("Clicked", "Outerwear clicked.");
+                        addNewOnClick("outerwear");
                         return false; // true to keep the Speed Dial open
                     case R.id.fab_add_top:
                         Log.d("Clicked", "Tops clicked.");
+                        addNewOnClick("top");
                         return false;
                     case R.id.fab_add_bottom:
                         Log.d("Clicked", "Bottoms clicked.");
+                        addNewOnClick("bottom");
                         return false;
                     case R.id.fab_add_footwear:
                         Log.d("Clicked", "Footwear clicked.");
+                        addNewOnClick("footwear");
                         return false;
                     default:
                         return false;
@@ -48,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
     //Displays outerwear in wardrobe on outerwear button click - if empty,
     //displays toast
@@ -75,8 +84,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //switches to add new clothing item activity
-    public void addNewOnClick(View v){
+    public void addNewOnClick(String type){
         Intent intent = new Intent(MainActivity.this, newClothingItem.class);
+        intent.putExtra("type", type);
         startActivity(intent);
     }
 }
